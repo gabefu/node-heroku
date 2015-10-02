0b677eb698781a28123d7826b842785fd257dbcd
gabefu <gabefueston@gmail.com>
2015-10-02 14:20:44 -0500

    added very simple form

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.render('pages/index');
-	response.writeHead(200, {"Content-Type": "text/plain"});
- 	response.end("Hello World\n");
+  response.render('pages/index');
+	//response.writeHead(200, {"Content-Type": "text/plain"});
+ 	//response.end("Hello World\n");
 });
 
+app.get('/foo', function(request, response) {
+
+	response.writeHead(200, {"Content-Type": "text/plain"});
+ 	response.end("foo");
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
