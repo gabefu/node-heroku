c9f70d2395548e1d0103b57766fff5ca64c16f46
gabefu <gabefueston@gmail.com>
2015-10-30 15:38:39 -0500

    made heroku app with form inputs to server, runs on localhost:5000

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use( bodyParser.urlencoded({extended: true}) );
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -10,6 +12,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  console.log(request);
   response.render('pages/index');
 	//response.writeHead(200, {"Content-Type": "text/plain"});
  	//response.end("Hello World\n");
